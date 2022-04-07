cc2676839dd8be90be6cc7694a3b9b8244e15959
mohibuslc <mohib_uslc@outlook.com>
2022-04-07 21:36:53 +0100

    Block , Scope

---
+++ block.js
@@ -0,0 +1,23 @@
+
+let block1 = 20
+
+function sum (first , second ){
+
+    let result = first + second + block1; 
+
+    if( result >9){
+
+        var mood = " Hankey ";
+
+        
+    }
+
+    console.log(mood)
+    return result ;
+   
+
+}
+
+const block = sum ( 7 , 3);
+
+console.log(block);
